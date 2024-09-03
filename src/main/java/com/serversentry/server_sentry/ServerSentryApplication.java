e90d50ea789f562de6d1c23ebd800a514a4c6026
souravmandal2004 <mandalsourav2004@gmail.com>
2024-09-03 22:49:36 +0530

    Project setup done

---
+++ src/main/java/com/serversentry/server_sentry/ServerSentryApplication.java
@@ -0,0 +1,13 @@
+package com.serversentry.server_sentry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSentryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerSentryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serversentry/server_sentry/ServerSentryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serversentry.server_sentry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSentryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
